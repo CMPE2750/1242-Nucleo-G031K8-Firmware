c08e0810b128da8238cc88571e071821e72268ae
Carlos Estay <75865555+carlosestay@users.noreply.github.com>
2025-01-29 08:52:23 -0700

    fix:/feat: Modified GPIO Library
    - Modified AF not working for GPIO higher than BIT 7
    - Added function for pull-up pull-down

--- Lib/inc/gpio.h
+++ Lib/inc/gpio.h
@@ -10,12 +10,47 @@
 #ifndef GPIO_H
 #define GPIO_H
 
+typedef enum GPIO_IO_ModeTypedef__
+{
+  IO_None = 0b00,
+  IO_PullUp = 0b01,
+  IO_PullDown = 0b10,
+  IO_Mask = 0b11
+}IO_Mode;
+
+  /// @brief Set GPIO pin as output
+  /// @param  GPIO Port
+  /// @param GPIO pin 
   void GPIO_InitInput(GPIO_TypeDef*, uint16_t);
+  /// @brief Set GPIO pin as input
+  /// @param  GPIO Port
+  /// @param GPIO pin  
   void GPIO_InitOutput(GPIO_TypeDef*, uint16_t);
+  /// @brief Set pull-up or pull-down
+  /// @param  GPIO Port
+  /// @param GPIO pin  
+  /// @param  mode
+  void GPIO_SetIO(GPIO_TypeDef*, uint16_t, IO_Mode);
+  /// @brief Set alternate function (AFx MUX)
+  /// @param  GPIO Port
+  /// @param GPIO pin   
+  /// @param AF 
   void GPIO_InitAlternateF(GPIO_TypeDef*, uint16_t, uint16_t);
+  /// @brief Set bit in Port
+  /// @param Port 
+  /// @param pin  
   void GPIO_Set(GPIO_TypeDef*, uint16_t);
+  /// @brief Clear bit in Port
+  /// @param Port 
+  /// @param pin  
   void GPIO_Clear(GPIO_TypeDef*, uint16_t);
+  /// @brief Toggle bit in Port
+  /// @param Port 
+  /// @param pin   
   void GPIO_Toggle(GPIO_TypeDef* , uint16_t);
+  /// @brief Reads pin in Port
+  /// @param Port 
+  /// @param pin       
   int GPIO_Read(GPIO_TypeDef*, uint16_t);
 
 #endif /* GPIO_H */
--- Lib/src/gpio.c
+++ Lib/src/gpio.c
@@ -41,6 +41,12 @@ void GPIO_InitOutput(GPIO_TypeDef* port, uint16_t pin)
   port->MODER |= (0b01 << (2*pin)); //Configure as output
 }
 //------------------------------------------------------------------------------
+void GPIO_SetIO(GPIO_TypeDef* port, uint16_t pin, IO_Mode mode)
+{
+  port->PUPDR &= ~((uint16_t)IO_Mask << (pin<<1));
+  port->PUPDR |= (uint16_t)mode << (pin<<1);
+}
+//------------------------------------------------------------------------------
   /*
     ALTERNATE FUNCTIONS (AF) see datasheet Tables 17 and 18
   - They go from AF0 to AF15 and they use 4 bits
@@ -58,8 +64,8 @@ void GPIO_InitAlternateF(GPIO_TypeDef* port, uint16_t bit, uint16_t function)
     }
     else if(bit < 16)
     {
-      port->AFR[1] &= ~(0xF << (bit*4));     //Clear AF setting
-      port->AFR[1] |= function << (bit*4);   //Write AF      
+      port->AFR[1] &= ~(0xF << ((bit-8)*4));     //Clear AF setting
+      port->AFR[1] |= function << ((bit-8)*4);   //Write AF      
     }
 }
 //------------------------------------------------------------------------------
